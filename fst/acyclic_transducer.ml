d10c02fe8b4ca67254ed8b2562346c6ba344d7da
Sidharth Kuruvila <sidharth.kuruvila@gmail.com>
2022-03-06 08:24:29 +1100

    feat: Add make_word and update_common_state_transition

--- fst/acyclic_transducer.ml
+++ fst/acyclic_transducer.ml
@@ -29,4 +29,28 @@ module Make(Fst: Fst_compiler) = struct
      let { ch; output; from_state } = temporary_state_transition in
      let from_state = State.set_transition from_state ch output compiled_next_state in
      compile_state from_state
+
+  let update_common_state_transition common_state_transition new_char remaining_output compiled_suffix_state =
+    let { ch; output; from_state } = common_state_transition in
+    let from_state = State.set_transition from_state ch output compiled_suffix_state in
+    { ch = new_char; output = remaining_output; from_state }
+
+  let make_word word output =
+    let n = String.length word in
+    let rec loop i =
+      if i = n then
+        []
+      else
+        let ch = String.get word i in
+        let temporary_state_transition = {
+          ch; output = Output.empty; from_state = State.init
+        } in
+        temporary_state_transition::loop (i+1) in
+    let first_char = String.get word 0 in
+    let first_temporary_state_transition = {
+      ch = first_char; output = output; from_state = State.init
+    } in
+    let remaining_temporary_state_transitions = loop 1 in
+    first_temporary_state_transition::remaining_temporary_state_transitions
+
 end
--- test-fst/test_acyclic_transducer.ml
+++ test-fst/test_acyclic_transducer.ml
@@ -34,7 +34,39 @@ let test_compile_temporary_state_transition () =
        return ()
   ))
 
+let test_update_common_state_transition () =
+  let module Fst = Fst.Make(String_output) in
+  let open Fst in
+  let module Builder = Acyclic_transducer.Make(Fst) in
+  let common_state_transition = { Builder.output = "output1"; ch = 'c'; from_state = { transitions = []; final_output = None } }in
+  let new_char = 'b' in
+  let remaining_output = "output2" in
+  let run_fst code = ignore (Fst.run 'a' 'b' code) in
+  run_fst (
+    let* compiled_suffix_state = State.empty_final_state ~empty_output:Output.empty |> Fst.compile_state in
+    let updated = Builder.update_common_state_transition common_state_transition new_char remaining_output compiled_suffix_state in
+    Alcotest.(check char) "The char in the updated state should be the new char" updated.Builder.ch new_char;
+    Alcotest.(check string) "The output in the udpated state should be the remaining_utput" updated.Builder.output remaining_output;
+    Alcotest.(check (option string)) "The original char and output should be pushed into the from_state"
+      (State.get_output updated.Builder.from_state 'c') (Some "output1");
+    return ()
+  )
+
+let test_make_word () =
+  let module Fst = Fst.Make(String_output) in
+  let module Builder = Acyclic_transducer.Make(Fst) in
+  let input = "input_word" in
+  let output = "output_word" in
+  let word = Builder.make_word input output in
+  let result_input = List.map (fun t -> t.Builder.ch) word |> List.to_seq |> String.of_seq in
+  let result_output = (List.hd word).output in
+  Alcotest.(check string) "The letters in constructed word should be those of the input" result_input input;
+  Alcotest.(check string) "The first transition of the word should containg the output" result_output output
+
+
 let tests = [
   "Push the output through a temporary transition", `Quick, test_push_output;
   "Compile temporary state transitions", `Quick, test_compile_temporary_state_transition;
+  "Update the common state transition", `Quick, test_update_common_state_transition;
+  "Convert the input into a list of temporary state transitions", `Quick, test_make_word;
 ]

aa5c3632843fe5d3a54e74e3707cfd65095c2cff
Sidharth Kuruvila <sidharth.kuruvila@gmail.com>
2022-04-05 02:43:26 +1000

    chore: Use a record to represent the byte array fst writers transducer

--- fst/byte_array_fst_compiler.ml
+++ fst/byte_array_fst_compiler.ml
@@ -7,7 +7,10 @@ module Make(Output: Output.S)(Output_writer: Output_writer.S
 
   module Writer = Byte_array_fst_writer.Make(Data_output)(Output)(Output_writer)
 
-  type transducer = (Buffer.t * int)
+  type transducer = {
+    buffer: Buffer.t;
+    last_node: int;
+  }
 
   module M = struct
     type 'a t = transducer -> ('a * transducer)
@@ -27,12 +30,12 @@ module Make(Output: Output.S)(Output_writer: Output_writer.S
 
   open Writer
 
-  let compile_state uncompiled_state (buffer, next_node) =
+  let compile_state uncompiled_state {buffer; last_node = next_node} =
     let { State.final_output; transitions } = uncompiled_state in
     if Lists.is_empty transitions then
       match final_output with
-      | None -> ((Output.empty, 0), (buffer, 0))
-      | Some final_output -> ((final_output, -1), (buffer,  -1))
+      | None -> ((Output.empty, 0), { buffer; last_node = 0 })
+      | Some final_output -> ((final_output, -1), { buffer; last_node = -1 })
     else
       let final_output = Option.value final_output ~default:Output.empty in
       let arcs = List.map (fun {State.ch; output; target = (next_final_output, target) } ->
@@ -41,10 +44,10 @@ module Make(Output: Output.S)(Output_writer: Output_writer.S
           final_output = next_final_output;
         }
       ) transitions in
-      let new_node = write_node buffer next_node arcs in
-      ((final_output, new_node), (buffer, new_node))
+      let last_node = write_node buffer next_node arcs in
+      ((final_output, last_node), {buffer; last_node})
 
   let run m =
-    let transducer = (Buffer.create 1024, -1) in
+    let transducer = { buffer=Buffer.create 1024; last_node = -1 } in
     m transducer |> snd
 end

55585c94b303e73aba22c92ac34f44662b645346
Sidharth Kuruvila <sidharth.kuruvila@gmail.com>
2022-04-07 11:35:06 +1000

    chore: Add missing block_pointer.mli

--- terms-enumerator/block_pointer.mli
+++ terms-enumerator/block_pointer.mli
@@ -1,7 +1,7 @@
 
 
 val find_block: string -> char -> int option
-(** [find_block pointer_data target_label] Find a block in the index to start scanning from.
+(** [find_block pointer_data target_label] finds a block in the index to start scanning from.
 
  The function returns None if the target label does not match anything.
  *)

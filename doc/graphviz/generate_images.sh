9910a80c1f5069d3360677a545c4ca063f7b5d4f
Sidharth Kuruvila <sidharth.kuruvila@gmail.com>
2022-04-30 05:40:20 +1000

    Update the docs

--- doc/graphviz/generate_images.sh
+++ doc/graphviz/generate_images.sh
@@ -1,5 +1,6 @@
 dot -Tpng fst-linear-scan.gv > ../images/fst-linear-scan.png
 dot -Tpng fst-direct-addressing.gv > ../images/fst-direct-addressing.png
+dot -Tpng fst-binary-search.gv > ../images/fst-binary-search.png
 dot -Tpng block-path.gv > ../images/block-path.png
 dot -Tpng dictionary-block.gv > ../images/dictionary-block.png
 dot -Tpng vint.gv > ../images/vint.png
